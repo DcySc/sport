d48e1fd7d12a199e6ae07b04ff51f31351ba04f6
DcySc <congyingdu00@gmail.com>
2019-03-31 13:24:19 +0800

    update

--- client/client/registration/src/app/detail/detail.page.ts
+++ client/client/registration/src/app/detail/detail.page.ts
@@ -7,6 +7,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DetailPage implements OnInit {
 
+  post = {
+    name: 'asda',
+    time: '2019/3/2 14:00',
+    address: '上师大东门',
+    message: '师大会受到哈苏东海USDhi阿湖湿地',
+    joiner: [{
+      name: 'sdasdas',
+      sex: '男',
+      hobby: '篮球'
+    },{
+      name: 'sadas',
+      sex: '男',
+      hobby: '足球'
+    },{
+      name: 'saddfsdf',
+      sex: '女',
+      hobby: '篮球'
+    }]
+  };
+
   constructor() { }
 
   ngOnInit() {
