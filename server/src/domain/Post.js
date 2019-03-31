c5cf0ce9bc68bcd1d77c9929fdf1b729d0975e66
chenl <131602111@qq.com>
2019-03-31 15:16:50 +0800

    update posrt domain

--- server/src/domain/Post.js
+++ server/src/domain/Post.js
@@ -3,7 +3,7 @@
  */
 module.exports = class Post{
   // jionList(参与者列表) user发起者id
-  constructor({id,title,details,date,jionList,user,beginDate}){
+  constructor({id,title,details,date,jionList,user,beginDate,address}){
     this.id = id;
     this.title = title;
     this.details = details;
@@ -11,5 +11,6 @@ module.exports = class Post{
     this.jionList = jionList;
     this.user = user;
     this.beginDate = beginDate;
+    this.address = address;
   }
 }
