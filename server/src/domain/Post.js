d321ad6fc65c16bac5ca866921c6a60cc88611ce
chenl <131602111@qq.com>
2019-03-31 17:40:28 +0800

    add

--- server/src/domain/Post.js
+++ server/src/domain/Post.js
@@ -3,12 +3,12 @@
  */
 module.exports = class Post{
   // jionList(参与者列表) user发起者id
-  constructor({id,title,details,date,jionList,user,beginDate,address}){
+  constructor({id,title,details,date,joinList,user,beginDate,address}){
     this.id = id;
     this.title = title;
     this.details = details;
     this.date = date;
-    this.jionList = jionList;
+    this.joinList = joinList;
     this.user = user;
     this.beginDate = beginDate;
     this.address = address;
